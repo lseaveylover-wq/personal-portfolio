d43bc7a8761699ebb2ced7c5e2a96b1e562fde18
l.seavey <l.seavey.lover@gmail.com>
2026-07-18 01:36:19 +0700

    Update portfolio

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
@@ -21,6 +22,7 @@ export default function RootLayout({
       <body className={`${inter.className} overflow-x-hidden`}>
         {children}
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
