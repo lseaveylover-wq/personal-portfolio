2944797f6a85f2489b930a8007ad8871918f6f38
l.seavey <l.seavey.lover@gmail.com>
2026-07-18 01:48:05 +0700

    Update portfolio

---
+++ global.d.ts
@@ -0,0 +1,9 @@
+declare module "*.css" {
+  const content: { [key: string]: string };
+  export default content;
+}
+
+declare module "*.scss" {
+  const content: { [key: string]: string };
+  export default content;
+}
